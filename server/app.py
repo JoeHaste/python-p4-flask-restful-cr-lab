ea1a533122c33fbd45bca5304b141ca42847de5d
Joseph Karanja <joseph.karanja@student.moringaschool.com>
2023-09-24 00:26:55 +0300

    complete assignement

--- server/app.py
+++ server/app.py
@@ -17,11 +17,95 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict_list = [p.to_dict() for p in Plant.query.all()]
+
+        response = make_response(
+            jsonify(response_dict_list),
+            200,
+        )
+        response.headers["Content-Type"] = "application/json"
+
+        return response
+    
+    def post(self):
+        new_plant = Plant(
+            name=request.form['name'],
+            image=request.form['image'],
+            price=request.form['price'],
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response_dict = new_plant.to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            200,
+        )
+        response.headers["Content-Type"] = "application/json"
+
+        return response
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        response_dict = Plant.query.limit(1).filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            200,
+        )
+        response.headers["Content-Type"] = "application/json"
+
+        return response
+
+    def put(self, id):
+        plant = Plant.query.limit(1).filter_by(id=id).first().to_dict()
+
+        for attr in request.form:
+            setattr(plant, attr, request.form.get(attr))
+
+        db.session.add(plant)
+        db.session.commit()
+
+        plant_dict = plant.to_dict()
+
+        response = make_response(
+            jsonify(plant_dict),
+            200,
+        )
+        response.headers["Content-Type"] = "application/json"
+
+        return response
+
+    def delete(self, id):
+        plant = Plant.query.limit(1).filter_by(id=id).first().to_dict()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        response_body = {
+            "deleted successfully":True,
+            "message": "Plant Deleted",
+        }
+
+        response_dict = response_body.to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            200,
+        )
+        response.headers["Content-Type"] = "application/json"
+
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')
+
         
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+      
